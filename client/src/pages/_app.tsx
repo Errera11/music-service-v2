46c244f41221cc389aeac2ac3c990c2f17cc6fff
Errera11 <forvitinn@mail.ru>
2023-08-04 17:59:43 +0300

    init

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Allerta } from 'next/font/google'
+const allerta = Allerta({
+  weight: "400",
+  subsets: ["latin"]
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={allerta.className}>
+    <Component {...pageProps} />
+  </main>
 }
