1f923c3cbbb7f5e607cc384c1c6d33d39ee7f663
Errera11 <forvitinn@mail.ru>
2023-09-13 19:10:21 +0300

    fix song service

--- server/src/core/serviceInterface/song/song.service.ts
+++ server/src/core/serviceInterface/song/song.service.ts
@@ -46,7 +46,6 @@ export class SongService implements SongRepository {
             },
         })
         const songsCount = songs.length;
-        })
         return {
             songs: await Promise.all(songs.map(async ({song: item}) => ({
                 ...item,
