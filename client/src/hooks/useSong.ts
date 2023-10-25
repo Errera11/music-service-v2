1d7eb4c5ef04775ebcad6614cf8d585f803222b6
Errera11 <forvitinn@mail.ru>
2023-10-25 22:29:11 +0300

    add skip/back song

--- client/src/hooks/useSong.ts
+++ client/src/hooks/useSong.ts
@@ -1,10 +1,31 @@
 import {Song} from "@/assets/types/Song";
 import {useAppDispatch} from "@/hooks/useAppDispatch";
 import {useTypedSelector} from "@/hooks/useTypedSelector";
+import {songActions} from "@/store/song";
+import {useCallback} from "react";
 import {playerActions} from "@/store/player";
 
-export function useSong(): [Song, (song: Song) => void] {
+export function useSong() {
     const dispatch = useAppDispatch();
-    const song = useTypedSelector(state => state.player);
-    return [song, (song: Song) => dispatch(playerActions.setSong(song))];
+    const song = useTypedSelector(state => state.songs.currentSong);
+    const {setCurrentTime} = playerActions;
+    const {skipNext, skipBack} = songActions;
+
+    const skipSong = useCallback(() => {
+        dispatch(setCurrentTime(0));
+        dispatch(skipNext());
+    }, [])
+
+    const skipBackSong = useCallback(() => {
+        song && dispatch(setCurrentTime(0));
+        dispatch(skipBack());
+    }, [])
+
+    const {setCurrentSong} = songActions;
+    return {
+        currentSong: song,
+        setSong: (song: Song) => dispatch(setCurrentSong(song)),
+        skipSong,
+        skipBackSong
+    }
 }
