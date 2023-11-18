1449723cb7239b749bd3eaaa8241a8604364d3b4
Errera11 <forvitinn@mail.ru>
2023-11-18 21:48:40 +0300

    optimized with player

--- client/src/components/songsList/SongsList.tsx
+++ client/src/components/songsList/SongsList.tsx
@@ -2,28 +2,17 @@ import React, {LegacyRef} from 'react';
 import {Song} from "@/assets/types/Song";
 import styles from './songsList.module.scss';
 import SongItem from '../song/Song';
-import SongPlate from "@/components/songPlate/SongPlate";
 
-const SongsList = ({songs, type, lastElementRef}: { songs: Song[], type: 'plate' | 'list', lastElementRef?: LegacyRef<any> }) => {
+const SongsList = ({songs, lastElementRef}: {
+    songs: Song[],
+    lastElementRef?: LegacyRef<any>
+}) => {
 
-    if(type === 'plate') return (
-        <div className={styles.container + ' ' + styles.plate}>
-            {songs.map((song, index) => {
-                if(index === (songs.length - 1) && lastElementRef) return <div ref={lastElementRef} key={song.id} className={styles.song}>
-                    <SongPlate song={song}/>
-                </div>
-
-                return <div key={song.id} className={styles.song}>
-                    <SongPlate song={song}/>
-                </div>
-            })}
-        </div>
-    );
-
-    else if(type === 'list') return (
+    return (
         <div className={styles.container}>
             {songs.map((song, index) => {
-                if(index === (songs.length - 1) && lastElementRef) return <div key={song.id} ref={lastElementRef} className={styles.song}>
+                if (index === (songs.length - 1) && lastElementRef) return <div key={song.id} ref={lastElementRef}
+                                                                                className={styles.song}>
                     <SongItem song={song}/>
                 </div>
                 return <div key={song.id} className={styles.song}>
@@ -32,8 +21,6 @@ const SongsList = ({songs, type, lastElementRef}: { songs: Song[], type: 'plate'
             })}
         </div>
     );
-
-    return <></>
 
 };
 
